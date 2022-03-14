12ebc443f003e745781b852287ea64d3b9b1f664
cindy <cindy.yeyuxin@gmail.com>
2022-03-13 22:02:18 -0700

    Enter key can add item to the list

--- app/src/main/java/com/example/iat366individualassignment/MainActivity.kt
+++ app/src/main/java/com/example/iat366individualassignment/MainActivity.kt
@@ -5,14 +5,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.CheckedTextView
-import android.widget.ImageButton
-import android.widget.Toast
+import android.view.*
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputEditText
@@ -36,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         val deleteButton = findViewById<Button>(R.id.deleteTask)
         val clearList = findViewById<Button>(R.id.clearList)
         val myRv = findViewById<RecyclerView>(R.id.myRv)
+        var input  = findViewById<TextInputEditText>(R.id.enterText)
+        var myScrollView = findViewById<ScrollView>(R.id.myScrollView)
+
         deleteItem = false
 
 
@@ -43,52 +40,52 @@ class MainActivity : AppCompatActivity() {
         myRv.layoutManager = LinearLayoutManager (this)
         myRv.adapter = MyAdapter(checkList, this)
 
+        //Function to add item into the arraylist
+        fun addItem () {
 
-
-        addButton.setOnClickListener {
-            var input  = findViewById<TextInputEditText>(R.id.enterText)
-            //var newText = findViewById<TextInputEditText>(R.id.enterText)
-
-            //If the input isn't empty, then add the content to the array
+            //If the input isn't empty then add the contents to the arraylist
             if (input.text.toString() != "") checkList.add(input.text.toString())
 
             //Notify the adapter
             myRv.adapter!! .notifyDataSetChanged()
 
             //Clear the input
-             input.setText("")
+            input.setText("")
+        }
 
+        //If the user hits the Enter Key, they can add the item
+        input.setOnKeyListener { v, keyCode, event ->
+
+            when {
+
+                ((keyCode == KeyEvent.KEYCODE_ENTER) && (event.action == KeyEvent.ACTION_DOWN)) -> {
+
+                    addItem()
+                    return@setOnKeyListener true
+                }
+                else -> false
+            }
+        }
+
+        //If the user clicks the add button, they can also add the item
+        addButton.setOnClickListener {
+            addItem()
         }
 
         //The delete button is a toggle. When you turn it on, anything you click on the list will be deleted whether it's checked or not.
-        //This is in case the user wants to delete the items they didn't check anyway
-        deleteButton.setOnTouchListener { view, motionEvent ->
+        deleteButton.setOnClickListener {
+            deleteItem = !deleteItem
 
-            view.performClick()
-
-            when (motionEvent.actionMasked) {
-                MotionEvent.ACTION_DOWN -> {
-                    deleteItem = !deleteItem
-
-                    //if delete: on, make the button fully colored and change the button text
-                    if (deleteItem) {
-                        deleteButton.setBackgroundColor(Color.argb(255, 255, 0, 0))
-                        deleteButton.setText("Delete: On")
-                    }
-                    //if delete: off, make button lighter color and change the button text
-                    if (!deleteItem) {
-                        deleteButton.setBackgroundColor(Color.argb(50, 255, 0, 0))
-                        deleteButton.setText("Delete: Off")
-                    }
-
-
-                    true
-                }
-
-                else -> true
+            //if delete: on, make the button fully colored and change the button text
+            if (deleteItem) {
+                deleteButton.setBackgroundColor(Color.argb(255, 255, 0, 0))
+                deleteButton.setText("Delete: On")
             }
-
-
+            //if delete: off, make button lighter color and change the button text
+            if (!deleteItem) {
+                deleteButton.setBackgroundColor(Color.argb(50, 255, 0, 0))
+                deleteButton.setText("Delete: Off")
+            }
         }
 
         clearList.setOnClickListener {
@@ -97,13 +94,7 @@ class MainActivity : AppCompatActivity() {
             checkList.clear()
             myRv.adapter!! .notifyDataSetChanged()
         }
-
-
     }
-
-
-
-
 }
 
 
